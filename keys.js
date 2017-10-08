23ad83f373c37d5ae1a2ab396af67bf2bf7d5058
BRIAN YACK <bdyack@gmail.com>
2017-10-08 11:38:16 -0500

    pushing more files

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: '<RLeuvZIBl0940pRtF3kdLYjwd>',
+  consumer_secret: '<khZM8u5sU8u0d794r8vG0X9plzCNORMCKLyHhhKInhlcsvZUuu>',
+  access_token_key: '<917064189499371521-flUMJtdMRIC59tmIRezklmGWPWLJUJM>',
+  access_token_secret: '<S4RKZ4xeI8ryu9HktP4GADL1QBioAdTfpMJIAzkIHQRYY>',
+}
+
+module.exports = twitterKeys;
